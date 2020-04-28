0d080a83cc0912f34d5adc34ef22384f2b31cbab
Prefol Timothy <timothy.prefol@gmail.com>
2020-04-28 20:04:03 +0200

    clean code

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -15,7 +15,6 @@ const update = (cache, payload) => {
     // manually update the cache on the client, so it matches the server
     // 1. Read the cache for the items we want
     const data = cache.readQuery({query: ALL_ITEMS_QUERY});
-    console.log(data, payload);
     // 2. Filter the deleted item out of the page
     // 3. Put the items back!
     cache.writeQuery({
